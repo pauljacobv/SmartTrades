03eaaf8492029d8cc498662bbe654ab3ae6fbae1
ashbabu <shyamashi@gmail.com>
2021-04-08 21:21:32 +0100

    update

--- fb_prophet.py
+++ fb_prophet.py
@@ -9,6 +9,7 @@ from nsepy import get_history
 from nsepy.derivatives import get_expiry_date
 from nsepy import get_index_pe_history
 
+
 class StockPredict:
 
     def __init__(self):
@@ -36,6 +37,9 @@ class StockPredict:
 
     def call_payoff(self, sT, strike_price, premium):
         return np.where(sT > strike_price, sT - strike_price, 0) - premium
+
+    def put_payoff(self, sT, strike_price, premium):
+        return np.where(sT < strike_price, strike_price - sT, 0) - premium
 
 
 if __name__ == '__main__':
@@ -90,27 +94,74 @@ if __name__ == '__main__':
 
     # Stock price range at expiration of the call
     sT = np.arange(0.95 * spot_price, 1.1 * spot_price, 1)
+    if st.checkbox('Bull Call Spread'):
+        col1, col2 = st.beta_columns(2)
+        col1.header('Long call')
+        col2.header('Short call')
+        strike_price_long_call = col1.number_input('enter strike_price_long_call', 1380, 10000)
+        strike_price_short_call = col2.number_input('enter strike_price_short_call', 1400, 10000)
+        col11, col22 = st.beta_columns(2)
+        premium_long_call = col11.number_input('enter premium for long call', 15, 100)
+        premium_short_call = col22.number_input('enter premium for short call', 10, 100)
+        payoff_long_call = sp.call_payoff(sT, strike_price_long_call, premium_long_call)
+        payoff_short_call = sp.call_payoff(sT, strike_price_short_call, premium_short_call) * -1.0
+        payoff_bull_call_spread = payoff_long_call + payoff_short_call
+        fig, ax = plt.subplots(figsize=(8, 5))
+        ax.spines['bottom'].set_position('zero')
+        ax.plot(sT, payoff_long_call, '--', label='Long Strike Call', color='g')
+        ax.plot(sT, payoff_short_call, '--', label='Short Strike Call ', color='r')
+        ax.plot(sT, payoff_bull_call_spread, label='Bull Call Spread')
+        plt.xlabel('Infosys Stock Price')
+        plt.ylabel('Profit and loss')
+        plt.legend()
+        plt.show()
+        st.pyplot(fig)
 
-    col1, col2 = st.beta_columns(2)
-    col1.header('Long call')
-    col2.header('short call')
-    strike_price_long_call = col1.number_input('enter strike_price_long_call', 1400, 10000)
-    strike_price_short_call = col2.number_input('enter strike_price_short_call', 1380, 10000)
-    col11, col22 = st.beta_columns(2)
-    premium_long_call = col11.number_input('enter premium for long call', 15, 100)
-    premium_short_call = col22.number_input('enter premium for short call', 10, 100)
-    payoff_long_call = sp.call_payoff(sT, strike_price_long_call, premium_long_call)
-    payoff_short_call = sp.call_payoff(sT, strike_price_short_call, premium_short_call) * -1.0
-    payoff_bull_call_spread = payoff_long_call + payoff_short_call
+    if st.checkbox('Bear Put Spread'):
+        col1, col2 = st.beta_columns(2)
+        col1.write('Long put')
+        col2.write('Short put')
+        strike_price_long_put = col1.number_input('enter strike_price_long_put', 1340, 10000)
+        strike_price_short_put = col2.number_input('enter strike_price_short_put', 1300, 10000)
+        col11, col22 = st.beta_columns(2)
+        premium_long_put = col11.number_input('enter premium for long put', 15, 100)
+        premium_short_put = col22.number_input('enter premium for short put', 10, 100)
+        payoff_long_put = sp.put_payoff(sT, strike_price_long_put, premium_long_put)
+        payoff_short_put = sp.put_payoff(sT, strike_price_short_put, premium_short_put) * -1.0
+        payoff_bull_put_spread = payoff_long_put + payoff_short_put
 
-    fig, ax = plt.subplots(figsize=(8, 5))
-    ax.spines['bottom'].set_position('zero')
-    ax.plot(sT, payoff_long_call, '--', label='Long Strike Call', color='g')
-    ax.plot(sT, payoff_short_call, '--', label='Short Strike Call ', color='r')
-    ax.plot(sT, payoff_bull_call_spread, label='Bull Call Spread')
-    plt.xlabel('Infosys Stock Price')
-    plt.ylabel('Profit and loss')
-    plt.legend()
-    plt.show()
-    st.pyplot(fig)
+        fig, ax = plt.subplots(figsize=(8, 5))
+        ax.spines['bottom'].set_position('zero')
+        ax.plot(sT, payoff_long_put, '--', label='Long Strike put', color='g')
+        ax.plot(sT, payoff_short_put, '--', label='Short Strike put ', color='r')
+        ax.plot(sT, payoff_bull_put_spread, label='Bull Call Spread')
+        plt.xlabel('Infosys Stock Price')
+        plt.ylabel('Profit and loss')
+        plt.legend()
+        plt.show()
+        st.pyplot(fig)
+
+    if st.checkbox('Protective Put'):
+        st.write('A protective put strategy is built by going long on a stock and simultaneously buying a put option.')
+        col1, col2, col3 = st.beta_columns(3)
+        col1.write('Stock purchase price')
+        col2.write('Strike price long put')
+        col3.write('Premium long put')
+        stoke_price = col1.number_input('enter stock purchase price', spot_price, 10000.0)
+        strike_price_long_put = col2.number_input('enter strike price short put', 1300.0, 10000.0)
+        premium_long_put = col3.number_input('enter premium for long put', 5.0, 100.0)
+        payoff_long_put = sp.put_payoff(sT, strike_price_long_put, premium_long_put)
+        payoff_stock = sT - stoke_price
+        payoff_protective_put = payoff_stock + payoff_long_put
+
+        fig, ax = plt.subplots(figsize=(8, 5))
+        ax.spines['bottom'].set_position('zero')
+        ax.plot(sT, payoff_stock, '--', label='Long Stock', color='g')
+        ax.plot(sT, payoff_long_put, '--', label='Long Strike Put', color='r')
+        ax.plot(sT, payoff_protective_put, label='Protective Put')
+        plt.xlabel('Stock Price')
+        plt.ylabel('Profit and loss')
+        plt.legend()
+        st.pyplot(fig)
+    st.write('https://optioncreator.com/')
     print('done')
